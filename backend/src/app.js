02e118c52360b0d912f93c365c602250bc5db182
umerr1214 <muhammedumer417@gmail.com>
2026-05-01 20:51:00 +0500

    build Ticket API (with validation + capacity control

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors";
 import eventRoutes from "./routes/eventRoutes.js";
+import ticketRoutes from "./routes/ticketRoutes.js";
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/events", eventRoutes);
+app.use("/tickets", ticketRoutes);
 
 export default app;
